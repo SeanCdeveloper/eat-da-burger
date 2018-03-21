c0ab9d254683f7871411688244b42dbdf34b4fc8
Maddie <maddiedeming1@gmail.com>
2018-03-21 12:14:42 -0400

    JAWSDB SETUP

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,19 @@
 // Global
 const mysql = require("mysql");
 // Connect
-const connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection;
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+else{
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+};
 connection.connect(function(err){
     if(err){
         console.error("error connecting: " + err.stack);
