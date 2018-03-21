cdde9ce040d05612a4f40bf85ffb54c985fb446e
Maddie <maddiedeming1@gmail.com>
2018-03-21 09:13:23 -0400

    Made it look prettier

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,19 @@
 // Global
 const mysql = require("mysql");
+require('dotenv').config();
 // Connect
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "burgers_db"
 });
 connection.connect(function(err){
     if(err){
-        //console.error("error connecting: " + err.stack);
+        console.error("error connecting: " + err.stack);
         return;
     }
-    //console.log("connected as id " + connection.threadId);
+    console.log("connected as id " + connection.threadId);
 });
 // Export Connection
 module.exports = connection;
--- server.js
+++ server.js
@@ -4,19 +4,16 @@ const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const methodOverride = require('method-override');
 const routes = require("./controllers/burgers_controller");
-// Express
 const app = express();
 const PORT = process.env.PORT || 8080;
-// Put Method
+// Config Settings
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: true }));
-// Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine","handlebars");
-// Routing + Public Folder
 app.use("/", routes);
 app.use(express.static('public'));
 // Listener
 app.listen(PORT,function(){
-    //console.log("App now listening at localhost:" + PORT);
+    console.log("App now listening at localhost:" + PORT);
 });
